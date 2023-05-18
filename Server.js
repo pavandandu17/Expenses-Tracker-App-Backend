da7299d74d7c23a9e76911807dd00016df73f251
pavandandu17 <pavandandu17@gmail.com>
2023-05-18 18:19:29 +0530

    Fixed Error

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 app.use(cors({
-    origin: ['https://etrackerapp.netlify.app', 'http://127.0.0.1:5500'];
+    origin: ['https://etrackerapp.netlify.app', 'http://127.0.0.1:5500']
 }));
 const AppData = require("./models/AppData");
 
